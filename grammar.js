0182e4329b1e182e21729a2a820603533ff3e961
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-02-09 19:25:03 +0800

    test top-level define

--- grammar.js
+++ grammar.js
@@ -2,8 +2,41 @@ module.exports = grammar({
   name: "racket",
 
   rules: {
-    source_file: ($) => $.language,
-    language: ($) => seq("#lang", $.identifier),
-    identifier: ($) => /[a-z]+/,
+    source_file: ($) => seq($.language, repeat($._top_level_form)),
+    language: ($) => seq("#lang", field("language", $.identifier)),
+    _top_level_form: ($) => choice($._expression, $.define),
+    define: ($) =>
+      seq(
+        "(",
+        "define",
+        choice(
+          seq(
+            "(",
+            field("name", $.identifier),
+            field("parameters", repeat($.identifier)),
+            ")",
+            $.body
+          ),
+          seq(field("name", $.identifier), $._expression)
+        ),
+        ")"
+      ),
+    body: ($) => seq(repeat($._statement), $._statement),
+    _statement: ($) => choice($.define, $._expression),
+    _expression: ($) =>
+      choice($.lambda, $.identifier, $.number, $.function_call),
+    function_call: ($) => seq("(", $._expression, repeat($._expression), ")"),
+    lambda: ($) =>
+      seq(
+        "(",
+        "lambda",
+        "(",
+        field("parameters", repeat($.identifier)),
+        ")",
+        $.body,
+        ")"
+      ),
+    number: ($) => /[0-9]+/,
+    identifier: ($) => /[a-zA-Z0-9\+\-\.\*\/<=>!?:$%_&~^\*]+/,
   },
 });
