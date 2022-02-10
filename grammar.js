4984a98218796ca5e8c33d0d920b9ca43e85bd47
Lîm Tsú-thuàn <dannypsnl@gmail.com>
2022-02-10 17:31:22 +0800

    all paren

--- grammar.js
+++ grammar.js
@@ -7,40 +7,52 @@ module.exports = grammar({
     _top_level_form: ($) => choice($._expression, $.define),
     define: ($) =>
       seq(
-        "(",
+        $._OPEN,
         "define",
         choice(
           seq(
-            "(",
+            $._OPEN,
             field("name", $.identifier),
             field("parameters", repeat($.identifier)),
-            ")",
+            $._CLOSE,
             $.body
           ),
           seq(field("name", $.identifier), $._expression)
         ),
-        ")"
+        $._CLOSE
       ),
     body: ($) => seq(repeat($._statement), $._statement),
     _statement: ($) => choice($.define, $._expression),
     _expression: ($) =>
       choice($.lambda, $.let, $.identifier, $.number, $.function_call),
-    function_call: ($) => seq("(", $._expression, repeat($._expression), ")"),
+    function_call: ($) =>
+      seq($._OPEN, $._expression, repeat($._expression), $._CLOSE),
     let: ($) =>
-      seq("(", "let", "(", repeat(seq("(", $.binding, ")")), ")", $.body, ")"),
+      seq(
+        $._OPEN,
+        choice("let", "let*", "letrec"),
+        $._OPEN,
+        repeat(seq($._OPEN, $.binding, $._CLOSE)),
+        $._CLOSE,
+        $.body,
+        $._CLOSE
+      ),
     binding: ($) => seq($.identifier, $._expression),
     lambda: ($) =>
       seq(
-        "(",
+        $._OPEN,
         "lambda",
-        "(",
+        $._OPEN,
         field("parameters", repeat($.identifier)),
-        ")",
+        $._CLOSE,
         $.body,
-        ")"
+        $._CLOSE
       ),
     // datum
     number: ($) => /[0-9]+/,
     identifier: ($) => /[a-zA-Z0-9\+\-\.\*\/<=>!?:$%_&~^\*]+/,
+    // parenthesis
+    _OPEN: ($) => choice("(", "[", "{"),
+    _CLOSE: ($) => choice(")", "]", "}"),
   },
 });
